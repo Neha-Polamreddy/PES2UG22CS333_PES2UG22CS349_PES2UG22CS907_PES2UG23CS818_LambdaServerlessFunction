106e2ad27a9e0d5c60081fbaa0215ec2f2c98356
Neha-Polamreddy <polamreddyneha2002@gmail.com>
2025-04-08 15:21:41 +0530

    Fixed models.py file

---
+++ models.py
@@ -0,0 +1,13 @@
+# backend/models.py
+from sqlalchemy import Table, Column, Integer, String
+from .database import metadata
+
+functions = Table(
+    "functions",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("name", String, nullable=False),
+    Column("route", String, nullable=False),
+    Column("language", String, nullable=False),
+    Column("timeout", Integer, nullable=False)
+)
