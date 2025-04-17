605dd865440615d6d842eb32e419ed8db3233458
Neha-Polamreddy <polamreddyneha2002@gmail.com>
2025-04-17 10:48:26 +0530

    updated workflow yml file

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,7 +2,7 @@
 
 import pytest
 from httpx import AsyncClient
-from backend.main import app  # Make sure this points to your FastAPI instance
+from backend.main import app  
 
 @pytest.mark.asyncio
 async def test_list_functions():
